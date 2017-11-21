4f39205aa5dd9a846d302e23286ac07a57354f15
Sébastien MORVAN <morvan.sebastien@gmail.com>
2017-11-22 00:17:38 +0100

    Modified to only support Windows 10 console

--- alphalight.py
+++ alphalight.py
@@ -1,5 +1,9 @@
 import sys
 import random
+import numpy
+import PIL
+import os
+import ctypes
 
 class ScreenSimulator:
 
@@ -14,17 +18,22 @@ class ScreenSimulator:
   def display(self):
     sys.stdout.write("\033[2J")
     sys.stdout.write("\033[1;31m")
+    #os.system('cls')
     i = self.w * self.h - 1
     for y in xrange(self.h):
       for x in xrange(self.w):
         if self.buffer[i] == 1: 
-          sys.stdout.write(u'\u25cf ')
+          #sys.stdout.write(u'\u25cf ')
+          sys.stdout.write('\xfe  ')
         else:
-          sys.stdout.write(u'  ')
+          sys.stdout.write('   ')
         i = i - 1
       print("")
 
-      
+
+kernel32 = ctypes.windll.kernel32
+kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
 s = ScreenSimulator(32, 8)
 for i in xrange(32*8):
   s.push(random.randint(0, 1))
