b56be38ebbe252a199fd152bbf81511eb4d74892
smorvan <smorvan>
2018-01-05 09:47:55 +0100

    Cleaned modules imports

--- alphalight.py
+++ alphalight.py
@@ -1,34 +1,23 @@
-from __future__ import print_function
-import sys
-import random
-import numpy
-import PIL
-import os
-import ctypes
 import time
-import string
-
-import string
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+import PIL
 import numpy as np
-from ledscreen import ScreenSimulator
-from ledscreen import utils
 from ledscreen import components
+from ledscreen import ScreenSimulator
 
 # See https://stackoverflow.com/questions/36384353/generate-pixel-matrices-from-characters-in-string
 # TODO Autoscroll auto center text component
 # TODO Time component
-  
+# TODO Weather component
+# TODO Manage fonts path in config file
+
 def test():
   w, h = (32, 8)  
 
-  scrollingText = components.AdaptativeTextComponent('Bonjour Elena et Jules !', 0, 0, 32, 8)  
+  scrollingText = components.AdaptativeTextComponent('Hello!', 0, 0, 32, 8)  
   s = ScreenSimulator.new(32, 8)
   
   while True:
-    image = Image.new('L', (w, h), 1) 
+    image = PIL.Image.new('L', (w, h), 1) 
     scrollingText.display(image) 
     arr = np.asarray(image)
     arr = np.where(arr, 0, 1)
