1d52d1e4843d25f81d490516d1cc40d194c22ac8
smorvan <smorvan>
2018-01-11 23:37:53 +0100

    Components are declared into a dedicated conf file

--- alphalight.py
+++ alphalight.py
@@ -15,13 +15,38 @@ from ledscreen import Displayer
 # TODO 
 # TODO Add a sequencer
 # TODO Manage RGBA colors
+# TODO Type controller enter/exit/+/-
 
+# Rules
+# 1: List of Widget
+# 2: For each widget, maybe something to do in backend
+# 3: 
+# 4: Configuration mode is the highest graphic priority
+# 5: Notifications preempt
 
+class ComponentManager():
+  
+  componentList = []
+
+  def load(self):
+    with open('./conf/components.conf') as f:
+      lines = f.readlines()
+    for line in lines:
+      c = eval(line)
+      self.componentList.append(c)   
+
+# Component Manager
+cm = ComponentManager()
+cm.load()
+
+# Screen
 screen = ScreenSimulator.new(32, 8)
+
+# Displayer
 d = Displayer.new(screen)
 
-scrollingText = components.AdaptativeTextComponent('Hello everybody!', 0, 0, 32, 8)  
-d.addComponent(scrollingText)
+for c in cm.componentList:
+  d.addComponent(c)
 
 d.start()
 
