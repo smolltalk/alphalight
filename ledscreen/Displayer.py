4966139c69033144f72e431113ab484988d60e60
Sebastien Morvan <smorvan@mail.ks.int>
2018-02-02 17:43:59 +0100

    Controller WIP

--- ledscreen/Displayer.py
+++ ledscreen/Displayer.py
@@ -4,10 +4,9 @@ import numpy as np
 import threading as th
 
 
-class Scheduler(th.Thread):
-    def __init__(self, component_manager, stopper=th.Event()):
+class ComponentSlider(object):
+    def __init__(self, component_manager):
         self.component_manager = component_manager
-        self.stopper = stopper
         self.component_list = []
         self.index = -1
 
@@ -22,31 +21,39 @@ class Scheduler(th.Thread):
         self.index %= size(self.component_list)
         return self.component_list[self.index]
 
-    def run(self):
-        c = self.next()
-        while not self.stopper.wait(.1):
-            self.ui(c)
 
-# TODO ui manager
-# TODO slider
-
-
-class Displayer(th.Thread):
-    def __init__(self, component_manager, screen, stopper=th.Event()):
-        self.component_manager = component_manager
-        self.screen = screen
-        self.component_list = []
+class PlayController(th.Thread):
+    def __init__(self, component_slider, stopper=th.Event()):
+        self.component_slider = component_slider
         self.stopper = stopper
-        th.Thread.__init__(self)
+        self.component = None
 
-    def display(self):
+    def stop(self):
+        self.stopper.set()
+
+    def compute_state(self):
+        if self.component is None:
+            self.component = self.component_slider.next()
+        else:
+
+    def run(self):
+        while not self.stopper.wait(.1):
+            has_changed = self.compute_state()
+            self.component.compute_ui(
+                self.input_reader, self.displayer, self.sound_player, has_changed)
+
+
+class Displayer(object):
+    def __init__(self, screen):
+        self.screen = screen
+
+    def display(self, component):
         self.component_list = self.component_manager.get_list()
 
         w, h = self.screen.size()
         image = PIL.Image.new('L', (w, h), 1)
 
-        for component in self.component_list:
-            component.display(image, None)
+        component.display(image, None)
 
         arr = np.asarray(image)
         arr = np.where(arr, 0, 1)
@@ -55,14 +62,3 @@ class Displayer(th.Thread):
             for b in a:
                 self.screen.push(b)
         self.screen.display()
-
-    def stop(self):
-        self.stopper.set()
-
-    def run(self):
-        while not self.stopper.wait(.1):
-            self.display()
-
-
-def new(component_manager, screen):
-    return Displayer(component_manager, screen)
