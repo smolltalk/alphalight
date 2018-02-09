e52cbaf90a74b8af0e460528114811e203311bf7
smorvan <morvan.sebastien@gmail.com>
2018-02-09 01:21:46 +0100

    Fix conflicts

--- ledscreen/Displayer.py
+++ ledscreen/Displayer.py
@@ -3,37 +3,82 @@ import PIL
 import numpy as np
 import threading as th
 
-class Displayer(th.Thread):
-  def __init__(self, component_manager, screen, stopper=th.Event()):
-    self.component_manager = component_manager
-    self.screen = screen
-    self.component_list = []
-    self.stopper = stopper
-    th.Thread.__init__(self)
+COMPONENT_COMPUTE_RATE = .1
 
-  def display(self):
-    self.component_list = self.component_manager.get_list()
 
-    w, h = self.screen.size()
-    image = PIL.Image.new('L', (w, h), 1)
+#
+#
+# Règles
+# - Le composant a une fonction draw qui renvoie une image
+# - C'est le composant supérieur qui dessine les composants enfants XYHW
+#   en appelant la fonction draw de ses enfants
+# - Le cache d'image est à la charge du composant
+# - Les coordonnées sont relatifs au parent
 
-    for component in self.component_list:
-      component.display(image, None)
+# component.draw()
+# 	// En fonction de w et h
+# 	return
 
-    arr = np.asarray(image)
-    arr = np.where(arr, 0, 1)
-    
-    for a in arr:
-      for b in a:
-        self.screen.push(b)
-    self.screen.display()
 
-  def stop(self):
-    self.stopper.set()
+#  - #0 : component.compute(displayer, refresh)
 
-  def run(self):
-    while not self.stopper.wait(.1):
-      self.display()
+#           displayer.draw()
+#
+# Question : détecter qu'il n'y a rien à faire
 
-def new(component_manager, screen):
-    return Displayer(component_manager, screen)
+
+class ComponentSlider(object):
+    def __init__(self, component_manager):
+        self.component_manager = component_manager
+        self.component_list = []
+        self.index = -1
+
+    def get_list(self):
+        self.component_list = self.component_manager.get_list()
+
+    def next(self, direction='r'):
+        if direction == 'r':
+            self.index += 1
+        else:
+            self.index -= 1
+        self.index %= size(self.component_list)
+        return self.component_list[self.index]
+
+
+class PlayController(th.Thread):
+    def __init__(self, component_slider, stopper=th.Event()):
+        self.component_slider = component_slider
+        self.stopper = stopper
+        self.component = None
+
+    def stop(self):
+        self.stopper.set()
+
+    def compute_state(self):
+        if self.component is None:
+            self.component = self.component_slider.next()
+        else:
+
+    def run(self):
+        while not self.stopper.wait(COMPONENT_COMPUTE_RATE):
+            has_changed = self.compute_state()
+            self.component.compute(self.displayer, has_changed)
+
+
+class Displayer(object):
+    def __init__(self, screen):
+        self.screen = screen
+
+    def display(self, widget):
+        w, h = self.screen.size()
+        image = PIL.Image.new('L', (w, h), 1)
+
+        image.paste(widget.draw(), widget.coords)
+
+        arr = np.asarray(image)
+        arr = np.where(arr, 0, 1)
+
+        for a in arr:
+            for b in a:
+                self.screen.push(b)
+        self.screen.display()
--- ledscreen/widget.py
+++ ledscreen/widget.py
@@ -9,6 +9,7 @@ import platform
 
 from ledscreen import utils
 
+<<<<<<< HEAD
 class Widget(object):
   def is_animated(self):
     return False
@@ -94,3 +95,87 @@ class AdaptativeTextWidget(Widget):
   
   def display(self, screenImage):
     self.widget.display(screenImage)
+=======
+
+class Widget(object):
+    x, y, w, h
+
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    @property
+    def coords(self)
+        return (x, y)
+
+
+class StaticImage(Widget):
+    def __init__(self, image, x, y, w, h):
+        super().__init__(x, y, w, h)
+        self.image = image
+        self.tw, self.th = self.image.size
+
+    def draw(self):
+        cropx = 0
+        cropy = 0
+        cropx2 = min(cropx + self.w, self.tw)
+        cropy2 = min(cropy + self.h, self.th)
+        cropTextImage = self.image.crop((cropx, cropy, cropx2, cropy2))
+        return cropTextImage
+
+
+class ScrollingImage(Widget):
+
+    def __init__(self, image, x, y, w, h):
+        super().__init__(x, y, w, h)
+        self.scrollOffset = 0
+        tw, th = image.size
+        self.scrollingImage = Image.new('L', (tw + w, th), 1)
+        self.scrollingImage.paste(image, (0, 0))
+        self.scrollingImage.paste(image, (tw, 0))
+        self.tw, self.th = self.scrollingImage.size
+
+    def draw(self):
+        cropx = self.scrollOffset
+        cropy = 0
+        cropx2 = min(cropx + self.w, self.tw)
+        cropy2 = min(cropy + self.h, self.th)
+        cropTextImage = self.scrollingImage.crop(
+            (cropx, cropy, cropx2, cropy2))
+        self.scrollOffset += 1
+        self.scrollOffset %= self.tw - self.w
+        return cropTextImage
+
+
+class AdaptativeImage(Widget):
+    def __init__(self, image, x, y, w, h):
+        super().__init__(x, y, w, h)
+        tw, th = image.size
+        if tw < w:
+            self.image = StaticImage(image, x, y, w, h)
+        else:
+            self.image = ScrollingImage(image, x, y, w, h)
+
+    def draw(self):
+        return self.image.draw()
+
+
+class AdaptativeText(Widget):
+    def __init__(self, text, x, y, w, h):
+        super().__init__(x, y, w, h)
+        #textImage = utils.text_to_image(text, '/Library/Fonts/Arial Bold.ttf', 10)
+        textImage = utils.text_to_image(
+            text, 'c:\\windows\\fonts\\arialbd.ttf', 10)
+        tw, th = textImage.size
+        if tw < w:
+            self.image = StaticImage(textImage, x, y, w, h)
+        else:
+            scrollingImage = Image.new('L', (tw + w, th), 1)
+            scrollingImage.paste(textImage, (0, 0))
+            self.image = ScrollingImage(scrollingImage, x, y, w, h)
+
+    def draw(self):
+        return self.image.draw(screenImage)
+>>>>>>> 64f20be7b7222c6f177d60e876d6553ed8f6f04c
